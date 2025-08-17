9f90aa2af86ef4e7e858f94fb85e9d24815d5c5c
Tomoe <osmanthus31172@gmail.com>
2025-08-17 16:52:55 +0900

    SWR組込み

--- src/app/_utils/user/UserProvider.tsx
+++ src/app/_utils/user/UserProvider.tsx
@@ -1,13 +1,13 @@
 'use client'
+// import { useRouter } from 'next/navigation'
+import React, { createContext, useCallback, useContext } from 'react'
 
-import { useRouter } from 'next/navigation'
-import React, { createContext, useCallback, useContext, useState } from 'react'
-
+import { useSessionSWR } from '@/app/_hooks/useSessionSWR'
 import type { AppUser } from '@/app/_types/user'
 
 type UserContextValue = {
   user: AppUser | null
-  setUser: (u: AppUser | null) => void
+  // setUser: (u: AppUser | null) => void
   refreshFromServer: () => void
 }
 
@@ -20,18 +20,18 @@ export function UserProvider({
   initialUser: AppUser | null
   children: React.ReactNode
 }) {
-  const [user, setUser] = useState<AppUser | null>(initialUser)
-  const router = useRouter()
+  // const [user, setUser] = useState<AppUser | null>(initialUser)
+  const { user, mutate } = useSessionSWR(initialUser)
+  // const router = useRouter()
 
   const refreshFromServer = useCallback(() => {
-    router.refresh()
-  }, [router])
+    // router.refresh()
+    // }, [router])
+    mutate()
+  }, [mutate])
 
-  return (
-    <UserContext.Provider value={{ user, setUser, refreshFromServer }}>
-      {children}
-    </UserContext.Provider>
-  )
+  // return <UserContext.Provider value={{ user,setUser, refreshFromServer }}>{children}</UserContext.Provider>
+  return <UserContext.Provider value={{ user, refreshFromServer }}>{children}</UserContext.Provider>
 }
 
 export function useSession() {
