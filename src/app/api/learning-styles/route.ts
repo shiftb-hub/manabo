0cfe6fe69fb1b9c5d5d528482d432651ea17fce1
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-29 12:20:46 +0900

    スタイル診断結果と相性のよいユーザーを取得するAPIを作成

--- src/app/api/learning-styles/route.ts
+++ src/app/api/learning-styles/route.ts
@@ -1,10 +1,86 @@
 import { NextRequest, NextResponse } from 'next/server' 
 
+import { CompatibleUser } from '@/app/(learning-style)/_types/compatibleUser'
+import { LearningStyleLists } from '@/app/(learning-style)/_types/learningStyleLists'
 import { LearningStyleRequest } from '@/app/(learning-style)/_types/learningStyleRequest' 
+import { StyleType } from '@/app/(learning-style)/_types/learningType'
 import { prisma } from '@/app/_lib/prisma' 
 import { getAuthUser } from '@/app/_utils/api/requireUser' 
 
-export const POST = async (req: NextRequest) => { 
+// 相性のよいTypeをマッピングするリスト
+const compatibleMap: Record<string, string[]> = {
+  forest: ['bamboo'],               // forestはbambooと相性◎
+  meadow: ['bamboo'],               // meadowはbambooと相性◎
+  bamboo: ['forest', 'meadow'],     // bambooはforest、meadowと相性◎
+}
+
+// DB保存している学習スタイルリストの取得 + 相性のよいユーザー抽出を行うAPI
+export const GET = async() => {
+  try {
+    const fetchLearningList = await prisma.learningStyle.findMany({
+      orderBy: {
+        id: 'asc'
+      },
+      select: {
+        id: true,
+        type: true,
+        name: true,
+        icon: true,
+        description: true,
+        characteristics: true,
+        recommendations: true,
+      }
+    })
+
+    // 取得したデータをstring→styleType型に変換する
+    const learningStyleLists: LearningStyleLists[] = fetchLearningList.map((style) => ({
+      ...style,
+      type: style.type as StyleType,
+    }));
+
+    // データベースから各typeと相性がいいユーザーを取得。
+    const compatibleUsersMap: Record<string, CompatibleUser[]> = {}
+
+    for (const [type, targetTypes] of Object.entries(compatibleMap)) {
+      const users = await prisma.user.findMany({
+        where: {
+          userLearningStyle: {
+            learningStyle: { type: { in: targetTypes} },
+          },
+        },
+        select: {
+          id: true,
+          nickName: true,
+          userLearningStyle: {
+            select: {
+              learningStyle: {
+                select: {
+                  type: true,
+                  name: true,
+                  icon: true,
+                }
+              },
+            },
+          },
+        },
+      })
+
+      // 各タイプごとに相性の良いユーザーをランダムで3人取得
+      const shuffled = users.sort(() => 0.5 - Math.random())
+      compatibleUsersMap[type] = shuffled.slice(0, 3) as CompatibleUser[]
+    }
+
+    return NextResponse.json({learningStyleLists, compatibleUsersMap}, { status: 200})
+  } catch(e) {
+    if (e instanceof Error) { 
+      return NextResponse.json({ message: e.message }, { status: 400 })
+    } 
+    return NextResponse.json({ message: 'Unknown error' }, { status: 500 })
+  }
+}
+
+// 学習スタイル診断結果を保存するためのAPI
+export const POST = async(req: NextRequest) => { 
   try { const user = await getAuthUser() 
     if (!user) return NextResponse.json({ message: 'Unauthorized'}, { status: 401 }) 
       
@@ -32,6 +108,7 @@ export const POST = async (req: NextRequest) => {
   } catch(e) {
     if (e instanceof Error) { 
       return NextResponse.json({ message: e.message }, { status: 400 })
-    } return NextResponse.json({ message: 'Unknown error' }, { status: 500 })
+    } 
+    return NextResponse.json({ message: 'Unknown error' }, { status: 500 })
   } 
 }
