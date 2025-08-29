4fd674fd6772ef9f69f1881c3a49b356f2db9231
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-29 12:26:04 +0900

    デバッグの削除

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,9 +5,7 @@ import { seedLearningStyles } from './seeds/learning_styles'
 const prisma = new PrismaClient()
 
 async function main() {
-  console.log('Seeding LearningStyles...')
   await seedLearningStyles()
-  console.log('LearningStyles seeded successfully.')
 }
 
 main()
