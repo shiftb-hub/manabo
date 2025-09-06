c47ef75960a2c378873b7b78cf0da3c2a88fdda2
Kazuya-Sakashita <kz0508@gmail.com>
2025-09-06 09:08:36 +0900

    AlertTitleにsr-onlyテキストを追加しa11yエラーを解消

--- src/app/_components/ui/alert.tsx
+++ src/app/_components/ui/alert.tsx
@@ -1,5 +1,5 @@
+import { type VariantProps, cva } from 'class-variance-authority'
 import * as React from 'react'
-import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/app/_lib/utils'
 
@@ -27,13 +27,15 @@ const Alert = React.forwardRef<
 ))
 Alert.displayName = 'Alert'
 
-const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
-  ({ className, ...props }, ref) => (
+const AlertTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, children, ...props }, ref) => (
     <h5
       ref={ref}
       className={cn('mb-1 font-medium leading-none tracking-tight', className)}
       {...props}
-    />
+    >
+      {children ?? <span className='sr-only'>アラート見出し</span>}
+    </h5>
   ),
 )
 AlertTitle.displayName = 'AlertTitle'
